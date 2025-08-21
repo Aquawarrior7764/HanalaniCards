3d2ea294572b0adfba1be55c63ca25c155569ea1
Aquawarrior7764 <157011952+Aquawarrior7764@users.noreply.github.com>
2025-08-20 20:21:01 -1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-export const APP_VERSION = 'beta 1.2.1';
-export const RECOVERY_URL = ''; // leave empty for now – we’ll add the username+email broker next
+export const APP_VERSION = 'beta 1.2.2';
+export const RECOVERY_URL = ''; // we’ll wire the username+email broker after this fix
